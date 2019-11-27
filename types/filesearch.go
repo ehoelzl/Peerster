7720e5256fa222bbb0e94acaf4a9a167c5a7c149
Edoardo Hölzl <edoardo.holzl@epfl.ch>
2019-11-27 14:35:22 +0100

    Add filesearch structs

---
+++ types/filesearch.go
@@ -0,0 +1,40 @@
+package types
+
+import (
+	"sync"
+	"time"
+)
+
+var duplicateThreshold int64 = 500
+
+type SearchRequests struct {
+	requests map[int64]*SearchRequest
+	sync.RWMutex
+}
+
+func (sr *SearchRequests) AddRequest(request *SearchRequest) bool {
+	sr.Lock()
+	defer sr.Unlock()
+
+	now := time.Now().UnixNano() / int64(time.Millisecond)
+	var todelete []int64
+	duplicated := false
+
+	for t, req := range sr.requests {
+		if now-t < duplicateThreshold {
+			if req.IsDuplicate(request) {
+				duplicated = true
+			}
+		} else {
+			todelete = append(todelete, t)
+		}
+	}
+
+	for _, t := range todelete {
+		delete(sr.requests, t)
+	}
+	if !duplicated {
+		sr.requests[now] = request
+	}
+	return !duplicated
+}
