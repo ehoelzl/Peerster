73572dc8db09368176fed65db49d47625df06dca
Edoardo Hölzl <edoardo.holzl@epfl.ch>
2019-12-02 11:48:58 +0100

    Update test

--- test_tlc.sh
+++ test_tlc.sh
@@ -5,15 +5,15 @@ cd ..
 go build
 
 sleep 1
-./Peerster -name=A -UIPort=8000 -gossipAddr=127.0.0.1:5000 -peers=127.0.0.1:5001 -rtimer=20 -N=4 > A.txt &
+./Peerster -name=A -UIPort=8000 -gossipAddr=127.0.0.1:5000 -peers=127.0.0.1:5001 -rtimer=20 -N=3 > A.txt &
 sleep 1
 
-./Peerster -name=B -UIPort=8001 -gossipAddr=127.0.0.1:5001 -peers=127.0.0.1:5002 -rtimer=20 -N=4 > B.txt &
+./Peerster -name=B -UIPort=8001 -gossipAddr=127.0.0.1:5001 -peers=127.0.0.1:5002 -rtimer=20 -N=3 > B.txt &
 sleep 1
 ./client/client -UIPort=8000 -file=duck.jpg
 
 sleep 10
-./Peerster -name=C -UIPort=8002 -gossipAddr=127.0.0.1:5002 -peers=127.0.0.1:5003 -rtimer=20 -N=4 > C.txt &
+./Peerster -name=C -UIPort=8002 -gossipAddr=127.0.0.1:5002 -peers=127.0.0.1:5003 -rtimer=20 -N=3 > C.txt &
 
 sleep 10
 
